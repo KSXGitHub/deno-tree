e137c26461804420df71c0093b63e563a07ed624
khai96_ <hvksmr1996@gmail.com>
2020-03-31 11:31:03 +0700

    Add tsdoc comments

--- async.ts
+++ async.ts
@@ -1,11 +1,23 @@
 import { join } from './deps.ts'
 
+/**
+ * Type of values that {@link traverseFileSystem} yields
+ */
 export interface Item {
+  /** Path to directory that contains current item */
   readonly container: string
+  /** Stat info and name of current item */
   readonly info: Deno.FileInfo
+  /** Wether current item is a directory */
   readonly isDirectory: boolean
 }
 
+/**
+ * Traverse a file system tree
+ * @param container Root of the tree
+ * @param deep Determine whether a particular directory should be explored
+ * @returns An async iterator of {@link Item}
+ */
 export async function * traverseFileSystem (
   container: string,
   deep: (param: Item) => boolean

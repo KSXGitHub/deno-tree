dbcee813b33c9e31164dfb8ce51a8434bb4697b8
khai96_ <hvksmr1996@gmail.com>
2020-03-31 08:37:22 +0700

    Create files

---
+++ async.ts
@@ -0,0 +1,27 @@
+import { join } from './deps.ts'
+
+export interface Item {
+  readonly container: string
+  readonly info: Deno.FileInfo
+  readonly isDirectory: boolean
+}
+
+export async function * traverseFileSystem (
+  container: string,
+  deep: (param: Item) => boolean
+): AsyncGenerator<Item> {
+  for (const info of await Deno.readdir(container)) {
+    const isDirectory = info.isDirectory()
+    const item: Item = {
+      container,
+      info,
+      isDirectory
+    }
+    yield item
+    if (isDirectory && deep(item)) {
+      yield * traverseFileSystem(join(container, info.name!), deep)
+    }
+  }
+}
+
+export default traverseFileSystem
